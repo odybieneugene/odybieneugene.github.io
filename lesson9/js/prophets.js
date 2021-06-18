40f2f21af13cd15d33d09e61fac0cb14c2815759
odybieneugene <ody.bieneugene@gmail.com>
2021-06-18 05:48:15 -0400

    Prophet page done

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -9,13 +9,22 @@ fetch(requestURL)
         for (let i = 0; i < prophets.length; i++) {
             let card = document.createElement('section');
             let h2 = document.createElement('h2');
+            let p1 = document.createElement('p');
+            let p2 = document.createElement('p');
             let img= document.createElement('img')
+            
+           
 
             h2.innerHTML = ` ${prophets[i].name}  ${prophets[i].lastname}`;
+            p1.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+            p2.textContent = 'Place of Birth: ' + prophets[i].birthplace;
             img.setAttribute('src', prophets[i].imageurl);
             img.setAttribute('Alt', `The official potrait of ${prophets[i].name} ${prophets[i].lastname}!`)
+         
 
             card.append(h2);
+            card.append(p1);
+            card.append(p2);
             card.append(img);
 
             document.querySelector('div.cards').append(card);
